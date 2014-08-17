9e99428b348aae6aebf1e46f829de8a0c3f21fa2
browles <rowlesben@gmail.com>
2014-08-16 18:45:38 -0700

    initial commit

--- surf.js
+++ surf.js
@@ -1,14 +1,17 @@
 var storage = new Firebase('https://fiery-torch-5161.firebaseio.com/');
 
-storage.on('value', function(dataSnapshot) {
+storage.on('child_changed', function(dataSnapshot) {
   var data = dataSnapshot.val(),
-      newUrl = data.url;
-  chrome.tabs.query({'active':true, 'lastFocusedWindow': true}, function(result) {
-    var id = result[0].id;
-    chrome.tabs.update(id, {url: newUrl});
-  });
+      newUrl = data.url,
+      room = data.room;
+  if (room === window._room) {
+    chrome.tabs.query({'active':true, 'lastFocusedWindow': true}, function(result) {
+      var id = result[0].id;
+      chrome.tabs.update(id, {url: newUrl});
+    });
+  }
 });
 
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-  storage.set({url: changeInfo.url});
+  storage.push({room: window._room, url: changeInfo.url});
 });
